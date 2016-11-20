977f2655ee2b585cb866aaeb84ed6e1d4549e401
Geng <gdjaccount@163.com>
2016-11-21 00:18:35 +0100

    Edited by WAN JINGWEI

--- app/src/main/java/it/polimi/chessofclans/pieces/Piece.java
+++ app/src/main/java/it/polimi/chessofclans/pieces/Piece.java
@@ -1,8 +1,10 @@
 package it.polimi.chessofclans.pieces;
 
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Created by gengdongjie on 17/11/2016.
@@ -24,12 +26,6 @@ public class Piece {
     private char color;
     private String position;
     private String initialPosition;
-
-
-    private int attackFromX;
-    private int attackFromY;
-    private int attackToX;
-    private int attackToy;
 
 
     public Piece(int iV, int mR, char mD, char mT, int aR, int aS, char aD, char c, String iP) {
@@ -115,61 +111,14 @@ public class Piece {
         }
     }
 
-//    public String getAttackCells() {
-//        String getAttackCells = "";
-//        int[][] oneOfCells = new int[4 * attackRange][2];
-//        for (int i = 1; i <= attackRange; i++) {
-//            if (attackDirections == '/') {
-////                 upward attack
-//                oneOfCells[4 * i - 4][0] = Integer.valueOf(position.substring(0, 1)) - i;
-//                oneOfCells[4 * i - 4][1] = Integer.valueOf(position.substring(1, 2));
-////                right attack
-//                oneOfCells[4 * i - 3][0] = Integer.valueOf(position.substring(0, 1));
-//                oneOfCells[4 * i - 3][1] = Integer.valueOf(position.substring(1, 2)) + i;
-////                downward attack
-//                oneOfCells[4 * i - 2][0] = Integer.valueOf(position.substring(0, 1)) + i;
-//                oneOfCells[4 * i - 2][1] = Integer.valueOf(position.substring(1, 2));
-////                left attack
-//                oneOfCells[4 * i - 1][0] = Integer.valueOf(position.substring(0, 1));
-//                oneOfCells[4 * i - 1][1] = Integer.valueOf(position.substring(1, 2)) - i;
-//            } else if (attackDirections == 'd') {
-////                upward right attack
-//                oneOfCells[4 * i - 4][0] = Integer.valueOf(position.substring(0, 1)) - i;
-//                oneOfCells[4 * i - 4][1] = Integer.valueOf(position.substring(1, 2)) + i;
-////                downward right attack
-//                oneOfCells[4 * i - 3][0] = Integer.valueOf(position.substring(0, 1)) + i;
-//                oneOfCells[4 * i - 3][1] = Integer.valueOf(position.substring(1, 2)) + i;
-////                downward left attack
-//                oneOfCells[4 * i - 2][0] = Integer.valueOf(position.substring(0, 1)) + i;
-//                oneOfCells[4 * i - 2][1] = Integer.valueOf(position.substring(1, 2)) - i;
-////                upward left attack
-//                oneOfCells[4 * i - 1][0] = Integer.valueOf(position.substring(0, 1)) - i;
-//                oneOfCells[4 * i - 1][1] = Integer.valueOf(position.substring(1, 2)) - i;
-//
-//
-//            }
-//        }
-//        for (int i = 0; i < 4; i++) {
-//            for (int j = 0; j < attackRange; j++) {
-////                    System.out.println("cells["+(i+4*j)+"][0]="+oneOfCells[i +4*j][0]);
-////                    System.out.println("cells["+(i+4*j)+"][1]="+oneOfCells[i +4*j][1]);
-//
-//                if ((oneOfCells[i + 4 * j][0] >= 1)
-//                        && (oneOfCells[(i + 4 * j)][0] <= 6)
-//                        && (oneOfCells[(i + 4 * j)][1] >= 1)
-//                        && (oneOfCells[(i + 4 * j)][1] <= 6)) {
-//                    getAttackCells = getAttackCells + oneOfCells[i + 4 * j][0] + oneOfCells[i + 4 * j][1];
-//                } else {
-//                    getAttackCells = getAttackCells + "xx";
-//                }
-//            }
-//        }
-//        return getAttackCells;
-//    }
 
-    public String getMoveCells(String boardState) {
+    public Set getMoveCells(String boardState) {
         int[][] boardStateInt = new int[6][6];
-        Set moveCellsSet = new HashSet();
+        Set moveCellsSet = new TreeSet();
+        Map moveCellsMapEnemy = new TreeMap();
+        Map moveCellsMap = new TreeMap();
+        Map moveCellsMapCopy = new TreeMap();
+//  initialize boardState
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 5; j++) {
                 if ((boardState.charAt(6 * i + j) >= 97) && (boardState.charAt(6 * i + j) <= 122)) {
@@ -181,81 +130,158 @@ public class Piece {
                 }
             }
         }
-
+//  initialize moveFromx,moveFromY
         int moveFromX = Integer.valueOf(position.substring(0, 1));
         int moveFromY = Integer.valueOf(position.substring(1, 2));
         String getMoveCells = "";
+//  For the piece whose move directions is any ,the available  move cells without move block condition
         if (moveDirections == 'a') {
             for (int i = 1; i <= 6; i++) {
                 for (int j = 1; j <= 6; j++) {
                     if (
-                            ((Math.pow(moveFromX - i, 2) + Math.pow(moveFromY - j, 2)) <= (2 * Math.pow(moveRange, 2)))
-                                    && ((Math.pow(moveFromX - i, 2) + Math.pow(moveFromY - j, 2)) != 0)
+
+                            (Math.pow(moveFromX - i, 2) + Math.pow(moveFromY - j, 2)) <= (2 * Math.pow(moveRange, 2))
+                                    && (boardStateInt[i - 1][j - 1] * boardStateInt[moveFromX - 1][moveFromY - 1] != 1)
                             ) {
-                        if (boardStateInt[i - 1][j - 1] * boardStateInt[moveFromX - 1][moveFromY - 1] != 1) {
-                            String k=""+i+j;
-                            moveCellsSet.add(k);
-//                            getMoveCells = getMoveCells + i + j;
+                        String k = "" + i + j;
+                        moveCellsSet.add(k);
+                    }
+                }
+            }
+//  remove cells in the situation of move block
+            String rCell = "";
+            int rCellX;
+            int rCellY;
+            moveCellsMap.put(position, 0);
+            String aroundCell = "";
+            for (int i = 1; i <= moveRange; i++) {
+                moveCellsMapCopy.clear();
+                moveCellsMapCopy.putAll(moveCellsMap);
+                Iterator it = moveCellsMapCopy.keySet().iterator();
+                while (it.hasNext()) {
+                    rCell = "" + it.next();
+                    rCellX = Integer.valueOf(rCell.substring(0, 1));
+                    rCellY = Integer.valueOf(rCell.substring(1, 2));
+                    for (int i1 = -1; i1 <= 1; i1++) {
+                        for (int j1 = -1; j1 <= 1; j1++) {
+                            aroundCell = "" + (rCellX + i1) + (rCellY + j1);
+                            if (moveCellsSet.contains(aroundCell)) {
+                                if (((moveFromX - 1 + i1) >= 0 && (moveFromX - 1 + j1) <= 5) && ((moveFromY - 1 + i1) >= 0 && (moveFromY - 1 + j1) <= 5)) {
+                                    if (moveCellsMap.get(aroundCell) == null) {
+                                        if (boardStateInt[moveFromX - 1][moveFromY - 1] * boardStateInt[rCellX - 1 + i1][rCellY - 1 + j1] == -1) {
+                                            if (!moveCellsMapEnemy.containsKey(aroundCell)) {
+                                                moveCellsMapEnemy.put(aroundCell, i);
+                                            }
+                                        } else {
+                                            moveCellsMap.put(aroundCell, i);
+                                        }
+                                    } else {
+                                        if (boardStateInt[moveFromX - 1][moveFromY - 1] * boardStateInt[rCellX - 1 + i1][rCellY - 1 + j1] == -1) {
+                                            if (!moveCellsMapEnemy.containsKey(aroundCell) || (Integer.valueOf(moveCellsMap.get(aroundCell).toString()) > i)) {
+                                                moveCellsMapEnemy.put(aroundCell, i);
+                                            }
+                                        } else if ((Integer.valueOf(moveCellsMap.get(aroundCell).toString()) > i)) {
+                                            moveCellsMap.put(aroundCell, i);
+                                        }
+//                                        else {System.out.println("repetitive cells");}
+                                    }
+                                }
+                            }
+//                            else {System.out.println("Drop");}
                         }
                     }
                 }
+                moveCellsMap.remove(position);
             }
-//            judgetment of move block
-            for (int i = 1; i <= moveRange; i++) {
-                Iterator it=moveCellsSet.iterator();
-                int cellsX;
-                int cellsY;
-                while(it.hasNext()){
-                    cellsX=Integer.valueOf(it.next().toString().substring(0,1));
-                    cellsY=Integer.valueOf(it.next().toString().substring(1,2));
-                    if ((Math.pow(cellsX-moveFromX,2)+Math.pow(cellsY-moveFromY,2))<=2){
-
-                    }
-
-                }
-
-            }
-
-
-        } else if (moveDirections == '/') {
+        }
+//  For the piece whose move directions is any ,the available  move cells without move block condition
+        else if (moveDirections == '/') {
             for (int i = 1; i <= 6; i++) {
                 for (int j = 1; j <= 6; j++) {
-                    if (((Math.abs(moveFromX - i) + Math.abs(moveFromY - j)) <= moveRange) && (Math.abs(moveFromX - i) + Math.abs(moveFromY - j)) != 0) {
-                        if (boardStateInt[i - 1][j - 1] * boardStateInt[moveFromX - 1][moveFromY - 1] != 1) {
-                            String k=""+i+j;
-                            moveCellsSet.add(k);
-                        }
+                    if ((Math.pow(moveFromX - i, 2) + Math.pow(moveFromY - j, 2)) <= (Math.pow(moveRange, 2)) && (boardStateInt[i - 1][j - 1] * boardStateInt[moveFromX - 1][moveFromY - 1] != 1)) {
+                        String k = "" + i + j;
+                        moveCellsSet.add(k);
                     }
                 }
             }
-        } else {
-            getMoveCells = "";
-            System.out.println("[Error]The moveDirections is illegal!");
+            if (moveType == 'w') {
+//  remove cells in the situation of move block
+                String rCell = "";
+                int rCellX;
+                int rCellY;
+                moveCellsMap.put(position, 0);
+                String aroundCell = "";
+                int[][] dir = new int[4][2];
+                dir[0][0] = -1;
+                dir[0][1] = 0;
+                dir[1][0] = 0;
+                dir[1][1] = 1;
+                dir[2][0] = 1;
+                dir[2][1] = 0;
+                dir[3][0] = 0;
+                dir[3][1] = -1;
+                for (int i = 1; i <= moveRange; i++) {
+                    moveCellsMapCopy.clear();
+                    moveCellsMapCopy.putAll(moveCellsMap);
+                    Iterator it = moveCellsMapCopy.keySet().iterator();
+                    while (it.hasNext()) {
+                        rCell = "" + it.next();
+                        rCellX = Integer.valueOf(rCell.substring(0, 1));
+                        rCellY = Integer.valueOf(rCell.substring(1, 2));
+                        for (int i1 = 0; i1 <= 3; i1++) {
+                            for (int j1 = 0; j1 <= 1; j1++) {
+                                aroundCell = "" + (rCellX + dir[i1][0]) + (rCellY + dir[i1][1]);
+                                if (moveCellsSet.contains(aroundCell)) {
+                                    if (((moveFromX - 1 + dir[i1][0]) >= 0 && (moveFromX - 1 + dir[i1][1]) <= 5) && ((moveFromY - 1 + dir[i1][0]) >= 0 && (moveFromY - 1 + dir[i1][1]) <= 5)) {
+
+                                        if (moveCellsMap.get(aroundCell) == null) {
+                                            if (boardStateInt[moveFromX - 1][moveFromY - 1] * boardStateInt[rCellX - 1 + dir[i1][0]][rCellY - 1 + dir[i1][1]] == -1) {
+                                                moveCellsMapEnemy.put(aroundCell, i);
+                                            } else {
+                                                moveCellsMap.put(aroundCell, i);
+                                            }
+                                        } else {
+                                            if (boardStateInt[moveFromX - 1][moveFromY - 1] * boardStateInt[rCellX - 1 + dir[i1][0]][rCellY - 1 + dir[i1][1]] == -1) {
+                                                if (!moveCellsMapEnemy.containsKey(aroundCell) || (Integer.valueOf(moveCellsMap.get(aroundCell).toString()) > i)) {
+                                                    moveCellsMapEnemy.put(aroundCell, i);
+                                                }
+                                            } else if ((Integer.valueOf(moveCellsMap.get(aroundCell).toString()) > i)) {
+                                                moveCellsMap.put(aroundCell, i);
+                                            }
+//                                            else {System.out.println("repetitive cells");}
+                                        }
+                                    }
+                                }
+//                            else {System.out.println("Drop");}
+                            }
+                        }
+                    }
+                    moveCellsMap.remove(position);
+                }
+            }
         }
 
-
-        return getMoveCells;
+        moveCellsMap.putAll(moveCellsMapEnemy);
+        System.out.println("moveCellsMap=" + moveCellsMap.toString());
+        System.out.println("moveCellsSet=" + moveCellsSet.toString());
+        System.out.println("moveCellsMapEnemy=" + moveCellsMapEnemy.toString());
+        return moveCellsSet;
     }
 
     public boolean judgeMove(String boardState, int moveToX, int moveToY) {
-        String getMoveCells = getMoveCells(boardState);
-        String moveTo = "" + moveToX + moveToY;
-
+        Set getMoveCells = getMoveCells(boardState);
+        String moveTo=""+moveToX+moveToY;
         boolean index = false;
-        for (int i = 0; i < (getMoveCells.length() / 2); i++) {
-            System.out.println(getMoveCells.substring(2 * i, 2 * i + 2));
-            System.out.println(moveTo);
-            if (getMoveCells.substring(2 * i, 2 * i + 2).equals(moveTo)) {
-
-                index = true;
-            }
+        if (getMoveCells.contains(moveTo)){
+            index =true;
         }
         return index;
 
     }
 
-    public String getAttackCells(String boardState) {
+    public Set getAttackCells(String boardState) {
         int[][] boardStateInt = new int[6][6];
+        Map attackCellsMap = new TreeMap();
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 5; j++) {
                 if ((boardState.charAt(6 * i + j) >= 97) && (boardState.charAt(6 * i + j) <= 122)) {
@@ -270,7 +296,6 @@ public class Piece {
 
         int attackFromX = Integer.valueOf(position.substring(0, 1));
         int attackFromY = Integer.valueOf(position.substring(1, 2));
-        String getAttackCells = "";
 
         if (attackDirections == '/') {
 
@@ -287,22 +312,20 @@ public class Piece {
 //            left attack
             dir[3][0] = 0;
             dir[3][1] = -1;
+            String aCell="";
             for (int i = 0; i <= 3; i++) {
                 for (int j = 1; j <= attackRange; j++) {
+
                     if (((attackFromX + dir[i][0] * j) < 1) || ((attackFromX + dir[i][0] * j) > 6) || ((attackFromY + dir[i][1] * j) < 1) || ((attackFromY + dir[i][1] * j) > 6)) {
-//                        System.out.println(attackFromX + dir[i][0] * j);
-//                        System.out.println(attackFromX + dir[i][1] * j);
-//                        System.out.println("1i="+i+" j="+j);
                         break;
                     } else if (boardStateInt[attackFromX - 1 + dir[i][0] * j][attackFromY - 1 + dir[i][1] * j] * boardStateInt[attackFromX - 1][attackFromY - 1] == -1) {
-//                        System.out.println("2i="+i+" j="+j);
-                        getAttackCells = getAttackCells + (attackFromX + dir[i][0] * j) + (attackFromY + dir[i][1] * j);
+                        aCell = ""+ (attackFromX + dir[i][0] * j) + (attackFromY + dir[i][1] * j);
+                        attackCellsMap.put(aCell,j);
+
                         break;
                     } else if (boardStateInt[attackFromX - 1 + dir[i][0] * j][attackFromY - 1 + dir[i][1] * j] * boardStateInt[attackFromX - 1][attackFromY - 1] == 1) {
-//                        System.out.println("3i="+i+" j="+j);
                         break;
                     } else {
-//                        System.out.println("4i="+i+" j="+j);
                     }
                 }
             }
@@ -322,12 +345,14 @@ public class Piece {
 //            upward right attack
             dir[3][0] = -1;
             dir[3][1] = -1;
+            String aCell="";
             for (int i = 1; i <= attackRange; i++) {
                 for (int j = 0; j <= 3; j++) {
                     if (((attackFromX + dir[j][0] * i) < 1) || ((attackFromX + dir[j][0] * i) > 6) || ((attackFromY + dir[j][1] * i) < 1) || ((attackFromY + dir[j][1] * i) > 6)) {
                         break;
                     } else if (boardStateInt[attackFromX - 1 + dir[j][0] * i][attackFromY - 1 + dir[j][1] * i] * boardStateInt[attackFromX - 1][attackFromY - 1] == -1) {
-                        getAttackCells = getAttackCells + (attackFromX + dir[i][0] * j) + (attackFromY + dir[i][1] * j);
+                        aCell = ""+ (attackFromX + dir[i][0] * j) + (attackFromY + dir[i][1] * j);
+                        attackCellsMap.put(aCell,j);
                         break;
                     } else if (boardStateInt[attackFromX - 1 + dir[j][0] * i][attackFromY - 1 + dir[j][1] * i] * boardStateInt[attackFromX - 1][attackFromY - 1] == 1) {
                         break;
@@ -337,25 +362,20 @@ public class Piece {
             }
         }
 
-        return getAttackCells;
+        return attackCellsMap.keySet();
     }
 
     public boolean judgeAttack(String boardState, int attackToX, int attackToY) {
-        String getAttackCells = getAttackCells(boardState);
-        String attackTo = "" + attackToX + attackToY;
-
+        Set getAttackCells = getMoveCells(boardState);
+        String moveTo=""+attackToX+attackToY;
         boolean index = false;
-        for (int i = 0; i < (getAttackCells.length() / 2); i++) {
-            System.out.println(getAttackCells.substring(2 * i, 2 * i + 2));
-            System.out.println(attackTo);
-            if (getAttackCells.substring(2 * i, 2 * i + 2).equals(attackTo)) {
-
-                index = true;
-            }
+        if (getAttackCells.contains(moveTo)){
+            index =true;
         }
         return index;
 
     }
+
 
 
     public boolean chooseActions(char action) {
